06250753abbe8a82bd06038201a5171a04b4e382
Changdae <einsvision@gmail.com>
2021-03-20 12:54:06 +0900

    reducer function에 store.dispatch({ type: 'add'}); action 보내기

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import { createStore } from 'redux';
 
 // reducer function은 나의 state (data)를 수정하는 곳이다.
 // 오직 하나의 reducer 함수가 state 수정을 관리한다!!! Redux의 강력함이다.
-const reducer = (state = 0) => {
+const reducer = (state = 0, action) => {
+  if(action.type === 'add'){
+    state += 1;
+  } else if (action.type === 'minus'){
+    state -= 1;
+  } else {
+    state += 0;
+  }
   return state;
 }
 
@@ -12,7 +19,18 @@ const store = createStore(reducer);
 console.log(store);
 console.log(store.getState());
 
+store.dispatch({ type: 'add'});
+console.log(store.getState());
 
+store.dispatch({ type: 'minus'});
+console.log(store.getState());
 
+store.dispatch({ type: 'add'});
+console.log(store.getState());
 
+store.dispatch({ type: 'add'});
+console.log(store.getState());
+
+store.dispatch({ type: 'add'});
+console.log(store.getState());
 
