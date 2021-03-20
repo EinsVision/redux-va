34d3e8e87d6bc0b4bf0f00c310bf05048027120b
Changdae <einsvision@gmail.com>
2021-03-20 22:32:08 +0900

    mapStateToProps

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import store from './routes/store';
 
 ReactDOM.render(
   <Provider store={store}>
+    {/* /이제 App 컴포넌트는 Store에 접근이 가능하다. */}
     <App />
   </Provider>, 
   document.getElementById("root")
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
+import { connect } from 'react-redux';
 
-function Home() {
+function Home({toDos}) {
   const [text, setText] = useState('');
 
   const onChange = (e) => {
@@ -19,8 +20,16 @@ function Home() {
         <input type="text" value={text} onChange={onChange}/>
         <button>Add</button>
       </form>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   )
 }
 
-export default Home
+// store로 부터 state를 가져다 줄것이다.
+function mapStateToProps(state){
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Home);
+
+// connect : 나의 components들을 store에 연결시켜줌 ( state, dispatch )
