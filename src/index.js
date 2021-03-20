57fb064319185655acbe432b4443102f466bd181
Changdae <einsvision@gmail.com>
2021-03-20 12:11:59 +0900

    vanilla counter

--- src/index.js
+++ src/index.js
@@ -1,45 +1,22 @@
-import { createStore } from 'redux';
-
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
+let value = parseInt(number.innerText);
 
-// reducer 나의 state (data) 를 수정하는 함수이다.
-const reducer = (state = 0, action) => {
-  if(action.type === 'ADD'){
-    console.log('reducer action is ADD')
-    state++;
-    console.log('state: ',state);
-    return state;
-  } else if (action.type === 'MINUS') {
-    console.log('reducer action is MINUS')
-    state--;
-    console.group('state: ', state);
-    return state;
-  } else {
-    return state;
-  }
-  
+const updateValue = () => {
+  number.innerText = value;
 }
 
-const store = createStore(reducer); //나의 data를 저장하는 곳.
-
-console.log('store: ', store.getState());
-
-// store.dispatch({type: 'ADD'});
-// store.dispatch({type: 'ADD'});
-// store.dispatch({type: 'ADD'});
-// store.dispatch({type: 'ADD'});
-// store.dispatch({type: 'MINUS'});
 
 const handleAdd = () => {
-  store.dispatch({ type: 'ADD '})
+  value += 1; 
+  updateValue();
 }
 
 const handleMinus = () => {
-  store.dispatch({ type: 'MINUS '})
+  value -= 1;
+  updateValue();
 }
-add.addEventListener('click', handleAdd );
-minus.addEventListener('click', handleMinus );
 
-console.log('store: ', store.getState());
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
