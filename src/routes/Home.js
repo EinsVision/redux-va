88dcdc7890687b1b2be74289b4e30a0389fa57d2
Changdae <einsvision@gmail.com>
2021-03-20 16:10:06 +0900

    building Home component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Home() {
+  const [text, setText] = useState('');
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(text);
+  }
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <h1>To Do lists</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange}/>
+        <button>Add</button>
+      </form>
+    </>
   )
 }
 
